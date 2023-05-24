329ce84479170e95ffdd5f12a837a33ef106e48e
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-05-23 21:19:31 -0300

    ajustado o cep response

--- src/BrasilAPI_Fast/BrasilAPICore/Models/CEPResponse.cs
+++ src/BrasilAPI_Fast/BrasilAPICore/Models/CEPResponse.cs
@@ -2,47 +2,54 @@
 
 namespace BrasilAPI;
 
-[DataContract]
+
+
+
 public record CEPResponse : BaseResponse
 {
-	[JsonPropertyName( "cep")]
 	public string CEP { get; set; }
-	
-	[JsonPropertyName( "state")]
+
 	[JsonConverter(typeof(JsonStringEnumConverter))]
-	public Uf UF { get; set; }
-	
-	[JsonPropertyName( "city")]
+	public Uf State { get; set; }
+
 	public string City { get; set; }
-	
-	[JsonPropertyName( "neighborhood")]
+
 	public string Neighborhood { get; set; }
-	
-	[JsonPropertyName( "street")]
+
 	public string Street { get; set; }
-	
-	[JsonPropertyName( "location")]
+
 	public Location Location { get; set; }
 }
 
+//string json = 
+//	$$"""
+//        {
+//   "CEP":"30310300",
+//   "UF":31,
+//   "City":"Belo Horizonte",
+//   "Neighborhood":"Sion",
+//   "Street":"Rua do Uruguai",
+//   "Location":{
+//      "Type":"Point",
+//      "Coordinates":{
+//         "Longitude":"-43.9313934",
+//         "Latitude":"-19.9519364"
+//      }
+//   }
+//}
+//""";
 
-[DataContract]
 public record Location
 {
-	[JsonPropertyName( "type")]
 	public string Type { get; set; }
-	
-	[JsonPropertyName( "coordinates")]
+
 	public Coordinates Coordinates { get; set; }
 }
 
 
-[DataContract]
 public record Coordinates
 {
-	[JsonPropertyName( "longitude")]
 	public string Longitude { get; set; }
-	
-	[JsonPropertyName( "latitude")]
+
 	public string Latitude { get; set; }
 }
